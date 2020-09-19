68a11e629754407e4f971a87722c035b1507df19
Pedro Colombano <pedromecf@gmail.com>
2020-09-19 16:02:42 -0300

    Handling exceptions and clearing screen

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -9,7 +9,7 @@ public class ChessPosition {
 	
 	public ChessPosition(char column, int row) {
 		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
-			throw new ChessException("Error at chess position: Valid values from a1 to h8");
+			throw new ChessException("Error at chess position. Valid values from a1 to h8");
 		}
 		this.column = column;
 		this.row = row;
